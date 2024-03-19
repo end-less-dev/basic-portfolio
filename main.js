4e974dbb3dd3f51e2518f59dad0a6a573b854c54
Sanjay <67906667+end-less-dev@users.noreply.github.com>
2024-03-20 01:56:00 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+AOS.init();
+
+// let year = prompt("enter the year");
+
+// if(year % 100 == 0 & year % 400 == 0){
+//   alert(year + "Its a leap year")
+// }else if(year % 4 == 0 & year % 100 != 0){
+//   alert(year + "its a leap year")
+// }else{
+//   alert("its not a leap year")
+// }
